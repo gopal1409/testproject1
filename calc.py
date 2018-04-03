73c09e03b92a3d7a9422f9182ea33ce14ffb5e46
gopal das <gopal1409@gmail.com>
2018-04-03 00:13:28 +0000

    initial commit

---
+++ calc.py
@@ -0,0 +1,19 @@
+dd(x, y):
+    """Add Function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+
+    return x / y
